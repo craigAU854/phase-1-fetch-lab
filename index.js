0fe4ab58829ff5742690c973c0a303d8f7045185
craigAU854 <craigaustin021@gmail.com>
2023-10-01 15:46:50 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((Response) =>Response.json())
+  .then((data) => renderBooks(data))
   // To pass the tests, don't forget to return your fetch!
   
 }
